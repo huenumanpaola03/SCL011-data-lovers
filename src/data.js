7551dc8d0a391820f416a94c399ab3e217d2349c
laboratoriaD305 <natalie.veloso.z@gmail.com>
2019-09-01 22:56:51 -0400

    select listo y se recorre la data

--- src/data.js
+++ src/data.js
@@ -3,8 +3,21 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+const lolData=Object.values(LOL)
+console.log(lolData);
 
-window.example = example;
+let result = lolData.filter(e =>{
+  return e.tags.includes(tags);
+})
+console.log(result);
+window.LOL=lolData;
+
+
+// (dataLovers,tags)=> { 
+// let result=dataLovers.filter(element => {
+//   return element.tags.includes(tags);
+// })
+// return result 
+// }
+// window.dataLovers=LOL;
+
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-document.getElementById("firts-page")
+window.onload =() =>{
+
+
+const lolData=Object.values(LOL);
+for (let i=0; i<lolData.lenght; i++){
+    document.getElementById("container").innerHTML+=`
+    <img src="${LOL[i].splash}">`
+  }
