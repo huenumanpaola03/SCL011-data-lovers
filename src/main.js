e25e7f93a88ee16c55d8f4d680678b526545af31
huenumanpaola03 <huenumanpaola03@gmail.com>
2019-09-04 13:06:00 -0300

    hj

--- src/main.js
+++ src/main.js
@@ -1,1 +1,48 @@
-/* Manejo del DOM */
+const lolData = Object.values(LOL.data);
+    
+
+let container = document.getElementById("root");
+const selectpartype = document.getElementById("options");
+
+
+
+document.addEventListener("load", () => {
+    createCards(lolData);
+});
+
+
+
+
+let createCards = (array) =>{
+    for (let i=0; i< array.length; i++)
+
+for (let i=0; i< lolData.length; i++); {
+
+    let cardContainer = document.createElement("div");
+ 
+    
+
+    let splash = document.createElement("img");
+   splash.setAttribute= splas
+   
+   
+
+
+
+    cardContainer.appendChild(splash);
+    container.appendChild(cardContainer).innerHTML; 
+}}
+
+
+
+
+selectpartype.addEventListener("change",() => {
+    const condition = document.getElementById("options").value;
+    let results = filterP(lolData, condition);
+    createCards(results);
+  
+})
+
+
+
+
