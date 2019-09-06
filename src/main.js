31ab48b274ad0d8f7bc775641e434c9d151bee24
laboratoriaD305 <natalie.veloso.z@gmail.com>
2019-09-06 09:02:58 -0400

    cartas en orden

--- src/main.js
+++ src/main.js
@@ -3,24 +3,33 @@ const lolData = Object.values(LOL.data);
 let container = document.getElementById("root");
 
 
-selecTags=addEventListener("change",()=>
- { 
-     const selecTags =document.getElementById("select-tags");
-    for (let index = 0; index < lolData.length; index++) {
- container.innerHTML+= ` 
+selecTags = addEventListener("change", () => {
+  const selecTags = document.getElementById("select-tags");
+  for (let index = 0; index < lolData.length; index++) {
+    container.innerHTML += ` 
  <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
+    <h3>Nombre: ${lolData[index].name}</h3s>
     <img class="imagen-lol" src="${lolData[index].splash}" >
     </div>
+
+
+
     <div class="flip-card-back">
-      <h1>Roles: ${lolData[index].info}</h1> 
+    <h1>${lolData[index].name}</h1>
+      <h4>Roles: ${lolData[index].tags}</h4> 
+      <h4>Tipo: ${lolData[index].partype}</h4> 
     
+        <h4> nivel de dicultad:${lolData[index].info.difficulty}</h4> 
     </div>
   </div>
-</div>
+</div>`
+  }
+})
 
-`
- }})
 
-
+document.getElementById("who").addEventListener("click", () => {
+  document.getElementById("firts-page").style.display = "none";
+  document.getElementById("second-page").style.display = "block";
+});
